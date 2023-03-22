53b6dc36aadcfd7f4523ddb76e12133457ba935e
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-03-22 14:32:02 -0300

    Update removerhtml.js

--- removerhtml.js
+++ removerhtml.js
@@ -1,1 +1,12 @@
 
+// Obtém o caminho atual da página
+var path = window.location.pathname;
+
+// Verifica se a página termina com ".html"
+if (path.endsWith('.html')) {
+  // Remove a extensão ".html"
+  var newPath = path.slice(0, -5);
+
+  // Redireciona para a nova página sem a extensão ".html"
+  window.location.replace(newPath);
+}
